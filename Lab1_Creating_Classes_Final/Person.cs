aedf1d439bf6e22900897bbb34c8ce601eba6434
Zhuo Xi Hong <100309969+DummiesAlert@users.noreply.github.com>
2023-01-27 10:49:42 -0700

    YAAAAAAAAAA

---
+++ Lab1_Creating_Classes_Final/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Creating_Classes_Final
+{
+    internal class Person
+    {
+
+        public int PersonID;
+        public int Age;
+        public string FName, LName, FavColor;
+        public bool IsWorking;
+        public Person(int PersonID, string FName, string LName, string FavColor, int Age, bool IsWorking)
+
+        {
+
+            this.PersonID = PersonID;
+            this.Age = Age;
+            this.FName = FName;
+            this.LName = LName;
+            this.FavColor = FavColor;
+            this.IsWorking = IsWorking;
+
+        }
+
+        public override string ToString()
+
+        {
+
+            string List = ($" PersonID: {PersonID}\nFirst Name: {FName}\nLast Name: {LName}\nFavorite Color: {FavColor}\nAge: {Age}\nIs Working? {IsWorking}");
+            return List;
+
+        }
+
+        public string DisplayPersonInfo()
+
+        {
+
+            string Info = ($"{PersonID}: {FName} {LName}'s FAVORITE COLOR is {FavColor}");
+            return Info;
+
+        }
+
+        public string ChangeFavoriteColor()
+
+        {
+            FavColor = "White";
+            string Change = DisplayPersonInfo();
+            return Change;
+        }
+
+        public string GetAgeInTenYears()
+
+        {
+
+            string AgeUp = ($"{FName} {LName}'s Age in 10 Years is: {Age + 10}");
+            return AgeUp;
+
+        }
+    }
+}
---
+++ Lab1_Creating_Classes_Final/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Creating_Classes_Final
+
+{
+
+    internal class Program
+
+    {
+
+        static void Main()
+
+        {
+
+            Person Ian = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person Gina = new Person(2, "Gina", "James", "Green", 18, false);
+            Person Mike = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person Mary = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            Console.WriteLine(Gina.DisplayPersonInfo());
+            Console.WriteLine(Mike.ToString());
+            Console.WriteLine(Ian.ChangeFavoriteColor());
+            Console.WriteLine(Mary.GetAgeInTenYears());
+
+        }
+
+    }
+
+}
